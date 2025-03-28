d1e94b84f16ae114899f98bf56452703cdae0af5
Conrad Wood <cnw@conradwood.net>
2025-03-28 16:44:33 +0000

    expose matrix count metrics
    Change-Id: I73b2008b4828a01b1734470f00634684fc0e3a8c

--- src/golang.conradwood.net/pinger/server/metrics.go
+++ src/golang.conradwood.net/pinger/server/metrics.go
@@ -5,10 +5,29 @@ import (
 	"time"
 
 	"golang.conradwood.net/go-easyops/authremote"
+	"golang.conradwood.net/go-easyops/prometheus"
 	"golang.conradwood.net/pinger/matrix"
 )
 
+var (
+	matrix_total_ctr = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "ping_matrix_total_checks",
+			Help: "V=2 U=none DESC=total checks executed for matrix",
+		},
+		[]string{"matrixname"},
+	)
+	matrix_fail_ctr = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "ping_matrix_failed_checks",
+			Help: "V=2 U=none DESC=checks failed for matrix",
+		},
+		[]string{"matrixname"},
+	)
+)
+
 func init() {
+	prometheus.MustRegister(matrix_total_ctr, matrix_fail_ctr)
 	go metrics_builder()
 }
 func metrics_builder() {
@@ -31,6 +50,9 @@ func build_metric() error {
 	}
 	for _, m := range ml.Matrices {
 		fmt.Printf("Matrix \"%s\": %d failed, %d ok\n", m.Name, m.Failed, m.Working)
+		l := prometheus.Labels{"matrixname": m.Name}
+		matrix_total_ctr.With(l).Set(float64(m.Failed + m.Working))
+		matrix_fail_ctr.With(l).Set(float64(m.Failed))
 	}
 	return nil
 }
